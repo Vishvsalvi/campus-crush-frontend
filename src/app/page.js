7e6b3a7e4d53bfb53d322fc0a74b0a4492e4a4da
Kachavi <vishaldineshbhaikacha@gmail.com>
2024-02-06 23:27:11 +0530

    select option fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,115 +1,14 @@
-"use client";
-
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-import { Button } from "@/components/ui/button";
-
-import { useState } from "react";
+import Forming from "@/components/Forming";
 
 export default function Home() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [gender, setGender] = useState("");
-  const [year, setYear] = useState("");
-  const [stream, setStream] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, password, year, stream);
-  };
+  // const [name, setName] = useState("");
+  //const [email, setEmail] = useState("");
+  //  const [password, setPassword] = useState("");
+  // const [gender, setGender] = useState("");
+  //const [year, setYear] = useState("");
+  //  const [stream, setStream] = useState("");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Card className="mt-20 mx-5">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl font-bold">
-            Welcome to Thakur College Exclusive Dating App 💖
-          </CardTitle>
-          <CardDescription className="text-center">
-            Enter your credentials to continue
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col space-y-4">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                name="name"
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-
-              <Label htmlFor="gender">Gender</Label>
-              
-              <Select>
-      <SelectTrigger className="">
-        <SelectValue
-        value={gender}
-        onChange={(e) => setGender(e.target.value)}
-        placeholder="Select your gender" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Gender</SelectLabel>
-          <SelectItem value="male">Male</SelectItem>
-          <SelectItem value="female">Female</SelectItem>
-         
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-               
-              
-
-              <Button
-                variant="destructive"
-                onClick={handleSubmit}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </main>
+  <Forming />
   );
 }
---
+++ src/components/Forming.jsx
@@ -0,0 +1,190 @@
+
+"use client";
+
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Button } from "@/components/ui/button";
+
+
+// import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+
+import { data, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+export default function Forming() {
+
+    const formSchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8),
+        gender: z.string(),
+    });
+
+    const form = useForm(
+        { resolver: zodResolver(formSchema) }
+    )
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between ">
+            <Card className="mt-20 mx-5">
+                <CardHeader>
+                    <CardTitle className="text-center text-2xl font-bold">
+                        Welcome to Thakur College Exclusive Dating App 💖
+                    </CardTitle>
+                    <CardDescription className="text-center">
+                        Enter your credentials to continue
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <div className="flex flex-col space-y-4">
+
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                <Label htmlFor="name">Name</Label>
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    {...field}
+                                                // value={name}
+                                                // onChange={(e) => setName(e.target.value)}
+                                                />
+
+                                            </FormControl>
+                                            <FormDescription />
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                >
+                                </FormField>
+
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                <Label htmlFor="email">Email</Label>
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    {...field}
+                                                // value={name}
+                                                // onChange={(e) => setName(e.target.value)}
+                                                />
+                                            </FormControl>
+                                            <FormDescription />
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                >
+                                </FormField>
+
+
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                <Label htmlFor="password">Password</Label>
+                                            </FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    {...field}
+                                                // value={name}
+                                                // onChange={(e) => setName(e.target.value)}
+                                                />
+                                            </FormControl>
+                                            <FormDescription />
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                >
+                                </FormField>
+
+                                <FormField
+                                    control={form.control}
+                                    name="gender"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>
+                                                <Label htmlFor="gender">Gender</Label>
+                                            </FormLabel>
+
+                                            <FormControl>
+                                                <Select onValueChange={field.onChange} >
+                                                    <SelectTrigger className="">
+                                                        <SelectValue
+                                                            // value={gender}
+                                                            // onChange={(e) => setGender(e.target.value)}
+                                                            placeholder="Select your gender" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        <SelectGroup>
+                                                            <SelectLabel>Gender</SelectLabel>
+                                                            <SelectItem value="male">Male</SelectItem>
+                                                            <SelectItem value="female">Female</SelectItem>
+
+                                                        </SelectGroup>
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormDescription />
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                >
+                                </FormField>
+
+
+                                <Button
+                                    variant="destructive"
+                                    // onClick={handleSubmit}
+                                    type="submit"
+                                >
+                                    Submit
+                                </Button>
+                            </div>
+                        </form>
+
+                    </Form>
+                </CardContent>
+            </Card>
+        </main>
+    );
+
+}
