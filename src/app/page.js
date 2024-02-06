843393f502df2b8a9a5dc79da3dd85dd8e743ff8
Vishv <vishvsalvi10@gmail.com>
2024-02-06 17:18:37 +0530

    Select tag not working need to be fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-"use client"
-import './globals.css'
+"use client";
+
 
 import {
   Card,
@@ -8,46 +8,47 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
 
+import { Button } from "@/components/ui/button";
 
-import { Button } from "@/components/ui/button"
-
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Home() {
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [year, setYear] = useState('')
-  const [stream, setStream] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState("");
+  const [year, setYear] = useState("");
+  const [stream, setStream] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(name
-      , email
-      , password
-      , year
-      , stream)
-  }
-
+    e.preventDefault();
+    console.log(name, email, password, year, stream);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Card className="mt-20 mx-5" >  
+      <Card className="mt-20 mx-5">
         <CardHeader>
-          <CardTitle className="text-center text-2xl font-bold" >Welcome to Thakur College Exclusive Dating App 💖</CardTitle>
-          <CardDescription className="text-center" >Enter your credentials to continue</CardDescription>
+          <CardTitle className="text-center text-2xl font-bold">
+            Welcome to Thakur College Exclusive Dating App 💖
+          </CardTitle>
+          <CardDescription className="text-center">
+            Enter your credentials to continue
+          </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
@@ -76,48 +77,39 @@ export default function Home() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <Label htmlFor="year">Year</Label>
-              <Select
-                id="year"
-                name="year"
-                value={year}
-                onChange={(e) => setYear(e.target.value)}
-              >
-                <SelectTrigger>
-                  <SelectValue>{year}</SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="First Year">First Year</SelectItem>
-                  <SelectItem value="Second Year">Second Year</SelectItem>
-                  <SelectItem value="Third Year">Third Year</SelectItem>
-                  <SelectItem value="Fourth Year">Fourth Year</SelectItem>
-                </SelectContent>
-              </Select>
-              <Label htmlFor="stream">Stream</Label>
-              <Select
-                id="stream"
-                name="stream"
-                value={stream}
-                onChange={(e) => setStream(e.target.value)}
-              >
-                <SelectTrigger>
-                  <SelectValue>{stream}</SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="Science">Science</SelectItem>
-                  <SelectItem value="Commerce">Commerce</SelectItem>
-                  <SelectItem value="Arts">Arts</SelectItem>
-                </SelectContent>
-              </Select>
-              <Button variant="destructive" onClick={handleSubmit} type="submit" >Submit</Button>
-            </div>
+
+              <Label htmlFor="gender">Gender</Label>
+              
+              <Select>
+      <SelectTrigger className="">
+        <SelectValue
+        value={gender}
+        onChange={(e) => setGender(e.target.value)}
+        placeholder="Select your gender" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Gender</SelectLabel>
+          <SelectItem value="male">Male</SelectItem>
+          <SelectItem value="female">Female</SelectItem>
          
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+               
+              
+
+              <Button
+                variant="destructive"
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </div>
           </form>
-
         </CardContent>
-
-
       </Card>
     </main>
-  )
+  );
 }
