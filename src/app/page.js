7e704956e894b2eeddbde50184b40a5a1d501017
Vishv <vishvsalvi10@gmail.com>
2024-02-08 22:07:17 +0530

    Fixed uncontrolled error and made new file structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,7 @@
 import Forming from "@/components/Forming";
 
 export default function Home() {
-  // const [name, setName] = useState("");
-  //const [email, setEmail] = useState("");
-  //  const [password, setPassword] = useState("");
-  // const [gender, setGender] = useState("");
-  //const [year, setYear] = useState("");
-  //  const [stream, setStream] = useState("");
-
+  
   return (
   <Forming />
   );
